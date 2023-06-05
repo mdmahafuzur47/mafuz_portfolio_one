2aab37435c26482daa5591561424ae7f2c2e687e
mdmahafuzur47 <mdmahafuzur4747@gmail.com>
2023-06-05 22:54:09 +0600

    Home page About Section Added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import About from "../../shared_pages/About/About";
 import Banner from "../../shared_pages/Banner/Banner";
+import Contact from "../../shared_pages/contact/Contact";
 import Footer from "../../shared_pages/footer/Footer";
 import Navbar from "../../shared_pages/navbar/Navbar";
 import Projects from "../../shared_pages/projects/Projects";
@@ -11,7 +12,7 @@ const Home = () => {
       <Banner />
       <About />
       <Projects />
-
+      <Contact />
       <Footer />
     </div>
   );
--- src/shared_pages/contact/Contact.jsx
+++ src/shared_pages/contact/Contact.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { FaLocationArrow } from "react-icons/fa";
+import { HiMailOpen } from "react-icons/hi";
 
 const Contact = () => {
-  return <div id="con">contact</div>;
+  return (
+    <div id="contact" className="max-w-5xl mx-auto py-28">
+      <h3 className="text-xl uppercase font-bold text-[#187EFA]">Contact</h3>
+      <h1 className="text-2xl font-semibold my-2">
+        Don't be shy! Hit me Up!👇
+      </h1>
+      <div className="flex gap-16 mt-5">
+        <div className="flex items-center gap-4">
+          <span className="p-5 text-3xl rounded-full shadow-2xl text-blue-900">
+            <FaLocationArrow />
+          </span>
+          <div>
+            <h1 className="text-xl font-semibold">Location</h1>
+            <p className="text-gray-600">Dhaka, Bangladesh</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <span className="p-5 text-3xl rounded-full shadow-2xl text-blue-900">
+            <HiMailOpen />
+          </span>
+          <div>
+            <h1 className="text-xl font-semibold">Mail</h1>
+            <p className="text-gray-600">mdmahafuzur4747@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
